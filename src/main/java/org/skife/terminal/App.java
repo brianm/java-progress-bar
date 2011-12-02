e96dff433865ba4ee554855b910bee3c317792fb
Brian McCallister <brianm@skife.org>
2011-12-01 21:34:15 -0700

    math with doubles is better

--- src/main/java/org/skife/terminal/App.java
+++ src/main/java/org/skife/terminal/App.java
@@ -1,29 +1,24 @@
 package org.skife.terminal;
 
-import jline.Terminal;
 import org.fusesource.jansi.Ansi;
 import org.fusesource.jansi.AnsiConsole;
 
-import java.io.IOException;
-import java.util.concurrent.ExecutionException;
 import java.util.concurrent.Future;
 
-/**
- * Hello world!
- */
+
 public class App
 {
-    public static void main(String[] args) throws IOException, InterruptedException, ExecutionException
+    public static void main(String[] args) throws Exception
     {
         AnsiConsole.systemInstall();
 
         System.out.println(Ansi.ansi().newline());
-        ProgressBar core = new ProgressBar(Label.create("/ning.0/db.core", 22),
+        ProgressBar core = new ProgressBar(Label.create("slower thing", 15),
                                            Height.fromBottom(0),
                                            15,
                                            Percentage.show());
 
-        ProgressBar rslv = new ProgressBar(Label.create("/ning.0/front.0/rslv.7", 22),
+        ProgressBar rslv = new ProgressBar(Label.create("fast thing", 15),
                                            Height.fromBottom(1),
                                            10,
                                            Percentage.show());
