ec5331cd2e4cb1e3fee7f0a5b6f79f55ddd1cf4c
Brian McCallister <brianm@skife.org>
2011-12-02 09:59:55 -0700

    remove unused import from example

--- src/main/java/org/skife/terminal/App.java
+++ src/main/java/org/skife/terminal/App.java
@@ -1,7 +1,5 @@
 package org.skife.terminal;
 
-import org.fusesource.jansi.AnsiConsole;
-
 import java.util.concurrent.Callable;
 import java.util.concurrent.ExecutorService;
 import java.util.concurrent.Executors;
